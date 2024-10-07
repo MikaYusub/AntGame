81e7a1d52618fcfd32ed5e01ca0931596382797c
Mikayil Yusubov <mikayil.yusubov@gmail.com>
2024-10-07 14:32:36 +0400

    fixed ui

--- Scenes/Level/Level.gd
+++ Scenes/Level/Level.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-@onready var healthBar = $CanvasLayer/HealthBar
-@onready var staminaBar = $CanvasLayer/StaminaBar
+@onready var healthBar = $Control/CanvasLayer/HealthBar
+@onready var staminaBar = $Control/CanvasLayer/StaminaBar
 @onready var player = $Player
 @onready var enemy_scene = preload("res://Scenes/Enemy/Enemy.tscn")
-@onready var time = $CanvasLayer/Score
+@onready var time = $Control/CanvasLayer/Score
 @onready var timer = $Timer
 
 
