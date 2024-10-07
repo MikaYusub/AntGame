1b3821a243f4c9097958b7d34d0fdc5f0ef33f31
Kamal Aghayev <agayev169@gmail.com>
2024-10-07 22:05:44 +0400

    Fix label name

--- Scenes/Level/MainMenu.gd
+++ Scenes/Level/MainMenu.gd
@@ -1,8 +1,8 @@
 extends Control
 
 func _on_play_pressed():
-    get_tree().change_scene_to_file("res://Scenes/Level/Level.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Level/Level.tscn")
 
 
 func _on_exit_pressed():
-    get_tree().quit()
+	get_tree().quit()
